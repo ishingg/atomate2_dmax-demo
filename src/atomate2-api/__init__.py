cf6604ea2faf838d0850235490f5a67b9f573da3
ishingg <ishing@ucsd.edu>
2025-09-10 15:36:30 -0700

    added CORS support to connect frontend to backend. NEEDS TO BE UPDATED BEFORE PUBLIC RELEASE

--- src/atomate2-api/__init__.py
+++ src/atomate2-api/__init__.py
@@ -8,6 +8,16 @@ from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
+
+
+# For CORS
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    return response
+    
 #SETTINGS = Atomate2Settings()
 
 import routes 
